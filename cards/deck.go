80ca9dbd137bad8b7b23c7d04fd87a300fc057bd
Patrick Goulding <patrick.goulding@nordstrom.com>
2021-09-01 18:09:21 -0700

    okay, thats good

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 // create a new type of 'deck' ... kinda
 // which is a slice of string
 
 type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
