19682b1dc718982be57d7f59b6f3053b4e77df54
Patrick Goulding <patrick.goulding@nordstrom.com>
2021-09-01 17:56:23 -0700

    thats good

---
+++ cards/deck.go
@@ -0,0 +1,6 @@
+package main
+
+// create a new type of 'deck' ... kinda
+// which is a slice of string
+
+type deck []string
--- cards/main.go
+++ cards/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 	for i, card := range cards {
 		fmt.Println(i, card)
